b5102feb7b5b6e7a6f16c67cedcb4a72191e07a8
eluvesi <114662724+eluvesi@users.noreply.github.com>
2025-05-17 17:22:49 +0300

    Добавлена защита от дубликатов. Улучшена работа подсказок для подстановки: теперь работает даже если вписывать новый тег в серидину строки.

--- remark_dialog.py
+++ remark_dialog.py
@@ -41,7 +41,7 @@ class RemarkDialog(QDialog):
             ]
             self.ui.categoryComboBox.addItems(categories)
             # Собираем все теги из родителя для автодополнения
-            all_tags = parent.get_all_tags()
+            all_tags = parent.get_tab_tags(0)  # tab_index = 0 -> собираем теги со вкладки "Все"
             # Создаём completer для подсказок при вводе
             completer = TagCompleter(all_tags, self.ui.tagsLineEdit)
             self.ui.tagsLineEdit.setCompleter(completer)
@@ -50,12 +50,44 @@ class RemarkDialog(QDialog):
         # В качестве выбранной категории устанавливаем текущую
         self.ui.categoryComboBox.setCurrentText(category)
         # В поле tagsLineEdit загружаем теги через запятую
-        self.ui.tagsLineEdit.setText(", ".join(tags))
+        self.ui.tagsLineEdit.setText(", ".join(tags) + (", " if tags else ""))
         # Подключаем кнопки
         self.ui.saveButton.clicked.connect(self.accept)
         self.ui.cancelButton.clicked.connect(self.reject)
-        # Устанавливаем фильтр событий для поля textEdit
+        # Реакция на изменение тегов (ввод запятой)
+        self.ui.tagsLineEdit.textChanged.connect(self.tags_changed)
+        # Фильтр событий для поля textEdit (реакция на Enter)
         self.ui.textEdit.installEventFilter(self)
+
+    def tags_changed(self):
+        """
+        Обрабатывает каждое изменение текста в поле ввода тегов.
+
+        Автоматически приводит весь текст к нижнему регистру после каждого изменения. При вводе запятой проверяет
+        список тегов на дубликаты и удаляет их, после чего перемещает курсор на ожидаемую позицию.
+        """
+        text = self.ui.tagsLineEdit.text()  # Считываем строку с тегами из tagsLineEdit
+        # Исправляем регистр - всё переводим в нижний
+        cursor_pos = self.ui.tagsLineEdit.cursorPosition()  # Получаем текущую позицию курсора в тесте
+        self.ui.tagsLineEdit.setText(text.lower())  # Приводим к нижнему регистру
+        self.ui.tagsLineEdit.setCursorPosition(cursor_pos)  # Возвращаем курсор на прежнее место
+        # Избавляемся от повторов - удаляем дубликаты
+        if cursor_pos == 0:
+            return
+        last_char = text[cursor_pos - 1]  # Узнаём последний введённый символ
+        # Если последний символ - не запятая, то пользователь продолжает ввод тега
+        if last_char != ",":
+            return
+        # Иначе только что целиком введён новый тег, проверяем всю строку тегов на регистр и дубликаты
+        tags = [tag.strip() for tag in text.split(",") if tag.strip()]  # Разделяем на теги по запятым
+        unique_tags = list(dict.fromkeys(tags))  # Убираем дубликаты
+        if unique_tags == tags:
+            return
+        # Формируем исправленную строку
+        new_text = ", ".join(unique_tags) + ", "  # Формируем новую строку
+        self.ui.tagsLineEdit.setText(new_text)  # Меняем текст
+        left_comma_pos = new_text.rfind(",", 0, cursor_pos)  # Вычисляем позицию последней запятой слева от курсора
+        self.ui.tagsLineEdit.setCursorPosition(left_comma_pos + 2)  # Курсор на два символа (", ") вправо от неё
 
     def get_data(self):
         """
@@ -68,7 +100,8 @@ class RemarkDialog(QDialog):
         """
         text = self.ui.textEdit.toPlainText().strip()  # Текст замечания считываем из поля textEdit
         category = self.ui.categoryComboBox.currentText().strip()  # Категорию считываем из categoryComboBox
-        tags = [tag.strip() for tag in self.ui.tagsLineEdit.text().split(",") if tag.strip()]  # Теги - из tagsLineEdit
+        tags = [tag.strip().lower() for tag in self.ui.tagsLineEdit.text().split(",") if tag.strip()]  # Собираем теги
+        tags = list(dict.fromkeys(tags))  # Убираем дубликаты тегов (они могут остаться несмотря на защиту при вводе)
         return text, category, tags
 
     def eventFilter(self, source, event):
@@ -121,8 +154,8 @@ class TagCompleter(QCompleter):
         """
         Формирует итоговую строку после выбора подсказки.
 
-        Метод вызывается автоматически, когда пользователь выбирает элемент из списка автодополнения.
-        Он подставляет выбранный тег вместо последнего фрагмента после запятой, сохраняя введённые ранее теги.
+        Метод вызывается автоматически, когда пользователь выбирает элемент из списка автодополнения. Заменяет
+        редактируемую часть строки на выбранный тег, даже если пользователь вставляет его в середину списка тегов.
 
         Аргументы:
             index (QModelIndex): Индекс выбранного элемента из списка подсказок.
@@ -132,16 +165,25 @@ class TagCompleter(QCompleter):
         """
         editor = self.widget()  # Узнаём виджет, с текстом из которого работаем
         current_text = editor.text()  # Получаем текущий текст в поле
-        parts = current_text.rsplit(',', 1)  # Разделяем по последней запятой
-        prefix = parts[0] + ', ' if len(parts) > 1 else ''  # Часть до запятой записываем в переменную prefix
-        return prefix + index.data()  # Возвращаем строку, где заменён только последний незавершённый тег
+        cursor_pos = editor.cursorPosition()  # Узнаём текущую позицию курсора
+        left_comma_pos = current_text.rfind(",", 0, cursor_pos)  # Узнаём позицию последней запятой слева от курсора
+        start = left_comma_pos + 1 if left_comma_pos != -1 else 0  # Запятой слева может не быть, если это первый тег
+        new_text = (
+                current_text[:start].rstrip() + " " + # Левый кусок до тега (без лишних пробелов справа)
+                index.data() + # Подсказка, выбранная пользователем
+                ", " + current_text[cursor_pos:].lstrip()  # Запятая и хвост после редактируемого тега (без пробелов)
+        )
+        tags = [tag.strip() for tag in new_text.split(",") if tag.strip()]  # Разбиваем строку на теги по запятым
+        unique_tags = list(dict.fromkeys(tags))  # Убираем дубликаты
+        result_text = ", ".join(unique_tags) + ", "  # Собираем обратно в строку с запятыми и пробелами
+        return result_text
 
     def splitPath(self, path):
         """
-        Определяет часть строки, по которой искать совпадения.
+        Определяет часть строки для поиска совпадений, исходя из позиции курсора.
 
-        Метод вызывается автоматически при каждом вводе текста. Возвращает только последний фрагмент
-        после запятой, чтобы QCompleter искал подсказки по нему, а не по всей строке.
+        Метод вызывается автоматически при каждом вводе текста. Возвращает фрагмент тега, который пользователь
+        редактирует в данный момент, даже если он добавляет новый тег в середину строки.
 
         Аргументы:
             path (str): Текущий ввод пользователя.
@@ -149,6 +191,9 @@ class TagCompleter(QCompleter):
         Возвращает:
             list[str]: Список из одного элемента — последнего слова для поиска совпадений.
         """
-        parts = path.rsplit(',', 1)  # Разделяем по последней запятой
-        last_tag = parts[-1].strip()  # Фрагмент после запятой
-        return [last_tag]  # Будем искать подсказки только по этому фрагменту
+        editor = self.widget()  # Узнаём виджет, с текстом из которого работаем
+        cursor_pos = editor.cursorPosition()  # Узнаём текущую позицию курсора
+        left_comma_pos = path[:cursor_pos].rfind(',')  # Узнаём позицию последней запятой слева от курсора
+        start = left_comma_pos + 1 if left_comma_pos != -1 else 0  # Запятой слева может не быть, если это первый тег
+        current_tag = path[start:cursor_pos].strip()  # Будем искать подсказки только по этому фрагменту
+        return [current_tag]
